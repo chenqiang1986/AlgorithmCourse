cef5fb613ed8d718257b9002f0d1ddd5cc74d8a4
Qiang Chen <chenqiang19860101@gmail.com>
2026-06-21 12:02:51 -0700

    x

---
+++ StudentUpload/QiangChen/dtb_range_sum_queries.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    long long n, q;
+    std::cin >>n >> q;
+
+    std::vector<long long> a(n,0);
+    for(long long i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    std::vector<long long> prefix_sum(n,0);
+    prefix_sum[0] =a[0];
+    for(long long i = 1; i < n; i++) {
+        prefix_sum[i] = prefix_sum[i-1] + a[i];
+    }
+
+    for (long long i = 0; i < q; i++) {
+        long long start, stop;
+        std::cin >> start >> stop;
+
+        long long sum = (start == 0) ? prefix_sum[stop] : prefix_sum[stop] - prefix_sum[start -1]; 
+        std::cout << sum << std::endl;
+    }
+}
